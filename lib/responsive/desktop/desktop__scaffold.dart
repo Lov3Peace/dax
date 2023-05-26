0e310836473c3fdc0e88ddfa396c5daa555febc4
Lov3Peace <xelaburton8@gmail.com>
2023-05-26 17:37:20 -0400

    created folders for diff platforms

--- lib/responsive/desktop/desktop__scaffold.dart
+++ lib/responsive/desktop/desktop__scaffold.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/main.dart';
 import 'package:flutter_application_1/util/profile_bubble.dart';
 import 'package:flutter_application_1/util/title_bubble.dart';
-import 'constants.dart';
+import '../constants.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});

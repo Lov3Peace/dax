2770ff308856f1777c22f763f985fd04f04927c5
Lov3Peace <xelaburton8@gmail.com>
2023-05-25 20:54:17 -0400

    test mobile PR

--- lib/responsive/mobile__scaffold.dart
+++ lib/responsive/mobile__scaffold.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/main.dart';
+import 'package:flutter_application_1/util/profile_bubble.dart';
+import 'package:flutter_application_1/util/title_bubble.dart';
+import 'constants.dart';
 
 class MobileScaffold extends StatefulWidget {
   const MobileScaffold({super.key});
@@ -12,7 +15,9 @@ class _MobileScaffoldState extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: tran,
+      extendBody: true,
+      appBar: myAppBar,
     );
   }
 }

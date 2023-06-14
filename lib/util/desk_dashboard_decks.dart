c078deb175ede3f0efb491383ade7df2eade6cfc
Lov3Peace <xelaburton8@gmail.com>
2023-06-13 20:40:08 -0400

    Fixed AppBar Staggered Issue and Started Desk Dash

--- lib/util/desk_dashboard_decks.dart
+++ lib/util/desk_dashboard_decks.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/responsive/constants.dart';
 
-deskDashboardDecks(proj, socs, fins, news) {
-  List decks = [proj, socs, fins, news];
+import 'decks.dart';
+
+deskDashboardDecks({required Widget lane1, Widget? lane2, Widget? lane3}) {
+  List decks = [lane1, lane2, lane3];
   return decks;
 }

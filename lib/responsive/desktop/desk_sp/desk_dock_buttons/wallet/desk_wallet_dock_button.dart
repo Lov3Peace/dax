69d515690ebfca4691c4ac5f5bfa772fc73b6041
Lex <l3x@Alira.local>
2024-07-16 13:13:20 -0400

    i dont even know at this point. Fixed these two file issues
    git status
    '

--- lib/responsive/desktop/desk_sp/desk_dock_buttons/wallet/desk_wallet_dock_button.dart
+++ lib/responsive/desktop/desk_sp/desk_dock_buttons/wallet/desk_wallet_dock_button.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
-import 'package:flutter_application_1/util/GlobalProvider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';

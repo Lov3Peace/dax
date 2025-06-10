ee4e144195a39f6b468290e17e1be6359119b329
Lov3Peace <xelaburton8@gmail.com>
2025-06-09 20:54:25 -0400

    login successful

--- node/controllers/authController.js
+++ node/controllers/authController.js
@@ -1,55 +1,69 @@
 import User from "../models/user.js";
-import CryptoJS from "crypto-js";
 import { errorLog, infoLog } from "../log.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 // Logger for info, debug, errors, etc.
 export const createUser = async (req, res) => {
-    // Creates a 'newUser' object and sets it equal to a 
-    // new instance of the 'User' model that we imported above 
-    // (the 'new' keyword is creating the new instance of the model
-    // which gives us more control over what we do with the model)
+	// Creates a 'newUser' object and sets it equal to a
+	// new instance of the 'User' model that we imported above
+	// (the 'new' keyword is creating the new instance of the model
+	// which gives us more control over what we do with the model)
 
-    try {
-        const hashedPw = await bcrypt.hash(req.body.password, 20);
-        const newUser = new User({
-            username: req.body.username,
-            password: hashedPw,
-            isAdmin: req.body.isAdmin
-        });
-        // Tries to save a newUser to the db and returns the 
-        // response in JSON format with the 201 status code.
-        // If it fails then return status code of 500 and 
-        // the error in JSON format
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-        infoLog.info(`User ${savedUser.username} Account Created Successfully`);
-    } catch (error) {
-        res.status(500).json(error);
-        console.log(error);
-        errorLog.error('error', error);
-    }
+	try {
+		const hashedPw = await bcrypt.hash(req.body.password, 10);
+		const newUser = new User({
+			username: req.body.username,
+			password: hashedPw,
+			email: req.body.email,
+			isAdmin: req.body.isAdmin,
+		});
+		// Tries to save a newUser to the db and returns the
+		// response in JSON format with the 201 status code.
+		// If it fails then return status code of 500 and
+		// the error in JSON format
+		const savedUser = await newUser.save();
+		infoLog.info(`User ${savedUser.username} Account Created Successfully`);
+		return res.status(201).json(savedUser);
+	} catch (error) {
+		errorLog.error("error", error);
+		return res.status(500).json(error);
+	}
 };
 
 export const loginUser = async (req, res) => {
-    try {
-        bcrypt.compare(req.body.username, hash, function(err, result) {
-            // result == true
-        });
-        bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-            // result == false
-        });
-    }
-    catch {
-    }
-}
+	try {
+		// debugger;
+		// const { username, password } = req.body;
+		const user = await User.findOne({ username: req.body.username });
+		if (!user) {
+			infoLog.info(`User ${user.username} not found`);
+			return res.status(404).json(user);
+		}
+		const hashedDbPw = user.password;
+		const matched = await bcrypt.compare(req.body.password, hashedDbPw);
+		if (matched) {
+			infoLog.info(`${user.username} logged in successfully`);
+			console.log(`${user.username} logged in Successfully`);
+			return res.status(200).json(`${user.username} logged in successfully`);
+		} else {
+			return res.status(401).json(err);
+		}
+	} catch (error) {
+		errorLog.error("error", error);
+		return res.status(401).json(`Login for user ${req.body.username} failed`);
+	}
+};
 export const deleteUser = async (req, res) => {
-    try {
-        const deletedUser = User.deleteOne({ username: req.body.username });
-        console.log(req.body.username);
-        res.status(200).json(deletedUser);
-    } catch (error) {
-        errorLog.error(error);
-        res.status(500).json(error);
-
-    }
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		if (!user) {
+			infoLog.info(`User ${user.username} not found`);
+			return res.status(404).json(user);
+		}
+		const deletedUser = await User.deleteOne({ username: user.username });
+		return res.status(200).json(deletedUser);
+	} catch (error) {
+		errorLog.error(error);
+		return res.status(500).json(error);
+	}
 };

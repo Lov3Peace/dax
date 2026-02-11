6f7d34bf5096ac0c1d983a725b03d813c2f5b652
Phil Park <davonp.parker@gmail.com>
2026-02-10 19:31:13 -0500

    Node updates

---
+++ node/routers/userDataRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {
+	getUserDashboardData,
+	getUsers,
+} from "../controllers/userController.js";
+
+const userDataRouter = Router();
+
+userDataRouter.get("/getUserDashboardData", getUserDashboardData);
+userDataRouter.get("/getUsers", getUsers);
+
+export default userDataRouter;
